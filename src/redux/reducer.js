33a2f0e44717573a01be3f69231ce29621d4e49b
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-05 23:41:36 +0300

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,29 +1,40 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-
-const contactsInitialState = []
+const contactsInitialState = [];
 
 const contactsReducer = (state = contactsInitialState, action) => {
-    switch (action.type) {
-        case "contacts/addContact":
-            return [...state, action.payload];
-        case "contacts/deleteContact":
-            return state.filter(contact => contact.id !== action.payload);
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case 'contacts/addContact':
+      // Перевіряємо чи є співпадіння імені серед доданих контактів і імені, що користувач хоче додати
+      const {name, number, id} = action.payload
+      const gotMatch = state.find(contact => {
+        return contact.name === name;
+      });
 
-const filterInitialState = ''
+      if (!gotMatch) {
+        // Асинхронно додаэмо новий контакт до масиву контактів в стані додатку
+        return [...state, { name, number, id }]
+      } else {
+        alert(`${name} already in list`);
+      }
+      return state
+    case 'contacts/deleteContact':
+      return state.filter(contact => contact.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const filterInitialState = '';
 
 const filterReducer = (state = filterInitialState, action) => {
-    switch (action.type) {
-        case "filter/setFilterQuery":
-            return action.payload
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case 'filter/setFilterQuery':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 export const rootReducer = combineReducers({
   contacts: contactsReducer,
