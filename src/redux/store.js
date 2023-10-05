1f29db1286811f8324fa366c5a08d92caa90f405
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-05 23:24:42 +0300

    add reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,7 @@
 import { createStore } from 'redux';
 import { devToolsEnhancer } from "@redux-devtools/extension";
-// Начальное значение состояния Redux для корневого редюсера,
-// если не передать параметр preloadedState.
-const initialState = {
-  contacts: [],
-  filter: '',
-};
+import { rootReducer } from './reducer';
 
-// Пока что используем редюсер который
-// только возвращает полученное состояние
-
-const rootReducer = (state = initialState, action) => {
-  return state;
-};
 
 // Создаем расширение стора чтобы добавить инструменты разработчика
 const enhancer = devToolsEnhancer();
