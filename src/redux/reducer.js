1a2a4bca28e941f35b79f2d0d47169d2015dd9be
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-06 02:30:43 +0300

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,16 +1,15 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-
-const contactsInitialState = [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}];
+const contactsInitialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 const contactsReducer = (state = contactsInitialState, action) => {
   switch (action.type) {
-    case 'contacts/addContacts':
-      return [...state, ...action.payload];
-
+    // ********************************************
     case 'contacts/addContact':
       // Перевіряємо чи є співпадіння імені серед доданих контактів і імені, що користувач хоче додати
       const { name, number, id } = action.payload;
@@ -25,36 +24,25 @@ const contactsReducer = (state = contactsInitialState, action) => {
         alert(`${name} already in list`);
       }
       return state;
-
+    // **************************************************
     case 'contacts/deleteContact':
       return state.filter(contact => contact.id !== action.payload);
-
-
-    case 'filter/filteringContacts':
-      if (action.payload === '') return state;
-      else {
-        const filtredState = state.filter(contact =>
-          contact.name.toLowerCase().includes(action.payload.toLowerCase())
-        );
-        return filtredState;
-      }
 
     default:
       return state;
   }
 };
 
-const filterInitialState = ''
+const filterInitialState = '';
 
 const filterReducer = (state = filterInitialState, action) => {
-    switch (action.type) {
-        case "filter/setFilterQuery":
-            return action.payload
-        default:
-            return state;
-    }
-}
-
+  switch (action.type) {
+    case 'filter/setFilterQuery':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 export const rootReducer = combineReducers({
   contacts: contactsReducer,
