c0f4abb56cce4af315b00aee6da78461b8a5a8ab
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-05 22:24:13 +0300

    Add actions

---
+++ src/redux/actions.js
@@ -0,0 +1,25 @@
+import { nanoid } from "nanoid";
+
+export const addContact = (name, number) => {
+  return {
+    type: "contacts/addContact",
+    payload: {
+      id: nanoid(),
+      name,
+      number,
+    },
+  };
+};
+export const deleteContact = contactId => {
+  return {
+    type: "contacts/deleteContact",
+    payload: contactId,
+  };
+};
+
+export const setFilterQuery = value => {
+  return {
+    type: "filter/setFilterQuery",
+    payload: value,
+  };
+};
