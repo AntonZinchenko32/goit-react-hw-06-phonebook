2d0cdae143f2d681b58cf44dd53576bccd52b54f
AntonZinchenko32 <zinchenko309@gmail.com>
2023-10-06 22:32:48 +0300

    library, storeConfigurate, index.js config

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
+// Slices
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+// Persist
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
+    contacts: contactsReducer,
+    filter: filterReducer
+  })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+  reducer: persistedReducer
 });
+
+export const persistor = persistStore(store)
+
+
